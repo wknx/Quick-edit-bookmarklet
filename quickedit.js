f23567c7b690ecf6ff0c8f8087ec66a6d6a01a1d
wknx <wally.knx@gmail.com>
2023-03-10 17:28:17 -0300

    Update quickedit.js

--- quickedit.js
+++ quickedit.js
@@ -1,7 +1,7 @@
 (function(){
   document.designMode='on';
   const s=document.createElement('style');
-  s.innerHTML=`body::before{content:'✏%EF%B8%8F Modo de Edição (aperte ESC pra encerrar)';z-index:9999;padding:10px;background:white;color:black;display:block;margin:10px;font-size:18px;border:2px solid orange;text-align:center;}`;
+  s.innerHTML=`body::before{content:'✏%EF%B8%8F Modo de Edição (aperte ESC pra concluir e fechar essa mensagem)';z-index:9999;padding:10px;background:white;color:black;display:block;margin:10px;font-size:18px;border:2px solid orange;text-align:center;}`;
   document.body.appendChild(s);
   window.scrollTo(0,0);
   document.addEventListener('keyup',e => {
